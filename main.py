f3ad3f706546c2cfca762004c6f6757d7543af9f
Kimi82 <cyrekamil@gmail.com>
2019-12-09 22:57:52 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,47 @@
 import requests
 from bs4 import BeautifulSoup
-URL = 'https://www.flashscore.pl/druzyna/wisla-krakow/rob20Q2Q/spotkania/'
+from flask import Flask, jsonify, request, render_template
+app = Flask(__name__)
+
+@app.route('/hello', methods=['GET', 'POST'])
+def hello():
+
+    # POST request
+    if request.method == 'POST':
+        print('Incoming..')
+        print(request.get_json())  # parse as JSON
+        return 'OK', 200
+
+    # GET request
+    else:
+        message = {'greeting':'Hello from Flask!'}
+        return jsonify(message)  # serialize and use JSON headers
+
+@app.route('/test')
+def test_page():
+    # look inside `templates` and serve `index.html`
+    return render_template('index.html')
+
+
+
+
 
 headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36"}
 
-page = requests.get(URL, headers=headers)
+def loadData():
+    page = requests.get(URL, headers=headers)
+    soup = BeautifulSoup(page.content, 'html.parser')
 
-soup = BeautifulSoup(page.content, 'htmk.parser')
+    title = soup.find(class_="product_information_title___2rG9M product_title gl-heading gl-heading--m").get_text()
+    price = soup.find(class_="gl-price__value").get_text()
+    print(title,price)
 
-matches = soup.find(class='sportName soccer')
+def getData():
+    URL = input("Take me url of website:")
+    return URL
+
+
+
+
+#URL = getData()
+#loadData()
